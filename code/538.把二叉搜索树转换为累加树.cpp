91ea3303f083d235c457ad29b61147c751fa7978
zyz <2718629413@qq.com>
2023-03-02 18:16:18 +0800

    add leetcode 538

---
+++ code/538.把二叉搜索树转换为累加树.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=538 lang=cpp
+ * @lcpr version=21708
+ *
+ * [538] 把二叉搜索树转换为累加树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+
+    TreeNode* convertBST(TreeNode* root) {
+        if (root) {
+            convertBST(root->right);
+            sum += root->val;
+            root->val = sum;
+            convertBST(root->left);
+        }
+
+        return root;
+    }
+};
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+
+/*
+// @lcpr case=start
+// 
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,null,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,0,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,4,1]\n
+// @lcpr case=end
+
+ */
+
+
