b64df5a8c44e7678b92164eb5bfa93cff73850c6
zyz <2718629413@qq.com>
2023-03-06 10:55:53 +0800

    add leetcode 1653

---
+++ code/1653/1653.使字符串平衡的最少删除次数1.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=1653 lang=cpp
+ * @lcpr version=21801
+ *
+ * [1653] 使字符串平衡的最少删除次数
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int minimumDeletions(string s)
+    {
+        int f = 0, cnt_b = 0;
+        for (char ch : s)
+        {
+            if (ch == 'b')
+                cnt_b++;
+            else
+                f = min(f + 1, cnt_b);
+        }
+
+        return f;
+    }
+};
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+/*
+// @lcpr case=start
+// "aababbab"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "bbaaaaabb"\n
+// @lcpr case=end
+
+ */
