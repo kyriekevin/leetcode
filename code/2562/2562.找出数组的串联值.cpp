c75036a4b83f5b8a2df32f7201df68bc48a941b6
kyriekevin <2718629413@qq.com>
2023-10-12 17:46:47 +0800

    add leetcode 2562 cpp code

---
+++ code/2562/2562.找出数组的串联值.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long findTheArrayConcVal(vector<int>& nums) {
+        long long res = 0;
+        for (int i = 0, j = nums.size() - 1; i <= j; i++, j--) {
+            if (i != j) res += stoi(to_string(nums[i]) + to_string(nums[j]));
+            else res += nums[i];
+        }
+
+        return res;
+    }
+};
