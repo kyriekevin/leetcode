9d913bf6fa38eeaf1ed38caa6382a7babdf084bb
kyriekevin <2718629413@qq.com>
2023-11-03 08:45:35 +0800

    [117.cpp] Add solution for connecting nodes in a binary tree

---
+++ code/0117/117.填充每个节点的下一个右侧节点指针II.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+  Node *connect(Node *root) {
+    if (!root)
+      return root;
+    auto cur = root;
+    while (cur) {
+      auto head = new Node(-1);
+      auto tail = head;
+      for (auto p = cur; p; p = p->next) {
+        if (p->left)
+          tail = tail->next = p->left;
+        if (p->right)
+          tail = tail->next = p->right;
+      }
+      cur = head->next;
+    }
+
+    return root;
+  }
+};
