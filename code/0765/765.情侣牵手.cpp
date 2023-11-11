0c4e8de359fe21352b5d734de3bbf605731dac0e
kyriekevin <2718629413@qq.com>
2023-11-11 21:03:56 +0800

    [Code] Add Solution for minSwapsCouples: Implements a method to find the minimum number of swaps required in a given array of couples.

---
+++ code/0765/765.情侣牵手.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  vector<int> p;
+
+  int find(int x) {
+    if (p[x] != x)
+      p[x] = find(p[x]);
+    return p[x];
+  }
+
+  int minSwapsCouples(vector<int> &row) {
+    int n = row.size() / 2;
+    for (int i = 0; i < n; i++)
+      p.push_back(i);
+
+    int cnt = n;
+    for (int i = 0; i < 2 * n; i += 2) {
+      int a = row[i] / 2, b = row[i + 1] / 2;
+      if (find(a) != find(b)) {
+        p[find(a)] = find(b);
+        cnt--;
+      }
+    }
+
+    return n - cnt;
+  }
+};
