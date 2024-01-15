89c33640d04a983c9f3e1e2da63b930ca5e98e6b
kyriekevin <2718629413@qq.com>
2024-01-15 21:35:00 +0800

    add leetcode 82 cpp code

---
+++ code/0082/82.remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=82 lang=cpp
+ * @lcpr version=30113
+ *
+ * [82] 删除排序链表中的重复元素 II
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(-1, head);
+        auto cur = dummy;
+        while (cur->next && cur->next->next) {
+            if (cur->next->val == cur->next->next->val) {
+                int val = cur->next->val;
+                while (cur->next && cur->next->val == val)
+                    cur->next = cur->next->next;
+            }
+            else cur = cur->next;
+        }
+
+        return dummy->next;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,3,4,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,1,1,2,3]\n
+// @lcpr case=end
+
+ */
+
