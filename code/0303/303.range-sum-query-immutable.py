3946acf873805fd8509bde3f5b81028b4ff60399
kyriekevin <2718629413@qq.com>
2024-03-18 09:43:14 +0800

    add 3.18 leetcode daily question python3 code

---
+++ code/0303/303.range-sum-query-immutable.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode.cn id=303 lang=python3
+# @lcpr version=30119
+#
+# [303] 区域和检索 - 数组不可变
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        n = len(nums)
+        self.s = [0] * (n + 1)
+        for i in range(n):
+            self.s[i + 1] = self.s[i] + nums[i]
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.s[right + 1] - self.s[left]
+
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# ["NumArray", "sumRange", "sumRange", "sumRange"][[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]\n
+# @lcpr case=end
+
+#
+
